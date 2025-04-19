c197c2e3184cc266659bad886da55afb9072ee7c
Ilias Almerekov <ilias.almerekov@db-n.com>
2025-04-19 16:41:38 +0200

    fix(stylelint): remove incompatible config-prettier for stylelint@16

--- .stylelint.config.mjs
+++ .stylelint.config.mjs
@@ -1,5 +1,14 @@
+// .stylelint.config.mjs
 export default {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: ['stylelint-config-standard'],
   plugins: [],
-  rules: {},
+  rules: {
+    'string-quotes': 'double',
+    indentation: 2,
+    'number-leading-zero': 'always',
+    'color-hex-case': 'lower',
+    'block-opening-brace-space-before': 'always',
+    'declaration-colon-space-after': 'always',
+    'declaration-colon-space-before': 'never',
+  },
 }
